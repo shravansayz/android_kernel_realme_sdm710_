34247fb36121552f30814e8bdea4ba2fe524f197
Sorayukii <sorayukii69@gmail.com>
2025-09-05 11:24:56 +0000

    fs: Implement KernelSU manual hooks
    reference: https://kernelsu.org/guide/how-to-integrate-for-non-gki.html#manually-modify-the-kernel-source
    
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>
    Signed-off-by: Abdallah Ibrahim <iabdollah40@gmail.com>

--- fs/stat.c
+++ fs/stat.c
@@ -119,6 +119,10 @@ int vfs_statx(int dfd, const char __user *filename, int flags,
 	ksu_handle_stat(&dfd, &filename, &flags);
 #endif
 
+#ifdef CONFIG_KSU
+	ksu_handle_stat(&dfd, &filename, &flags);
+#endif
+
 	if ((flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		       AT_EMPTY_PATH | KSTAT_QUERY_FLAGS)) != 0)
 		return -EINVAL;
